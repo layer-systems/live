09e415c9226c3dba7e3b4ed5341fd9b7cf0baf63
PascalR <24775431+mroxso@users.noreply.github.com>
2023-06-30 13:10:53 +0200

    redirect to nostrapp.link

--- nostr.js
+++ nostr.js
@@ -21,6 +21,7 @@ async function nostrGetStreams() {
         let streaming = ""; 
         let status = "";
         let d = "";
+        let id = data.id;
 
         for(let i = 0; i < data.tags.length; i++) {
             if(data.tags[i][0] == ('title')) {
@@ -43,54 +44,71 @@ async function nostrGetStreams() {
             }
         }
 
+        if(status == "live") {
+            // Create a random div number
+            const divNumber = Math.floor(Math.random() * 999999);
+            // Create the outer div with class "card"
+            const cardDiv = document.createElement("div");
+            cardDiv.classList.add("card");
 
-        // Create a random div number
-        const divNumber = Math.floor(Math.random() * 999999);
-        // Create the outer div with class "card"
-        const cardDiv = document.createElement("div");
-        cardDiv.classList.add("card");
+            // Create the inner div with class "card-body"
+            const cardBodyDiv = document.createElement("div");
+            cardBodyDiv.classList.add("card-body");
 
-        // Create the inner div with class "card-body"
-        const cardBodyDiv = document.createElement("div");
-        cardBodyDiv.classList.add("card-body");
+            // Create the sender placeholder h5 element with class "card-title"
+            const senderH5 = document.createElement("h5");
+            senderH5.classList.add("card-title");
+            senderH5.textContent = pubkey;
+            senderH5.id = pubkey+""+divNumber;
 
-        // Create the sender placeholder h5 element with class "card-title"
-        const senderH5 = document.createElement("h5");
-        senderH5.classList.add("card-title");
-        senderH5.textContent = pubkey;
-        senderH5.id = pubkey+""+divNumber;
+            // Create the lightning bolt icon element
+            const lightningIcon = document.createElement("i");
+            lightningIcon.classList.add("bi", "bi-lightning-fill");
 
-        // Create the lightning bolt icon element
-        const lightningIcon = document.createElement("i");
-        lightningIcon.classList.add("bi", "bi-lightning-fill");
+            // Create the p element with the satoshi amount and lightning icon
+            const satoshiP = document.createElement("p");
+            satoshiP.textContent = summary;
+            satoshiP.prepend(lightningIcon);
 
-        // Create the p element with the satoshi amount and lightning icon
-        const satoshiP = document.createElement("p");
-        satoshiP.textContent = summary;
-        satoshiP.prepend(lightningIcon);
+            // Create the receiver placeholder h5 element with class "card-title"
+            // const receiverH5 = document.createElement("h5");
+            // receiverH5.classList.add("card-title");
+            // receiverH5.textContent = streaming;
+            // ===================
+            // const receiverH5 = document.createElement("video");
+            // receiverH5.classList.add("card-title");
+            // receiverH5.id = "video"+divNumber;
+            // receiverH5.src = streaming;
+            // const videoSource = document.createElement("source");
+            // videoSource.src = streaming;
+            // receiverH5.appendChild(videoSource);
+            // ===================
+            const idEncoded = NostrTools.nip19.noteEncode(id);
+            const nostrAppLink = "https://nostrapp.link/#"+idEncoded;
+            const receiverH5 = document.createElement("a");
+            receiverH5.href = nostrAppLink;
+            receiverH5.target = "_blank";
+            receiverH5.textContent = "Watch";
+            // receiverH5.classList.add("card-title");
+            receiverH5.textContent = "Watch";
 
-        // Create the receiver placeholder h5 element with class "card-title"
-        const receiverH5 = document.createElement("h5");
-        receiverH5.classList.add("card-title");
-        receiverH5.textContent = streaming;
+            const dateDiv = document.createElement("div");
+            dateDiv.classList.add("card-date");
+            dateDiv.textContent = formattedCreatedAt + " - " + status;
 
-        // Create the date placeholder div element with class "card-date"
-        const dateDiv = document.createElement("div");
-        dateDiv.classList.add("card-date");
-        dateDiv.textContent = formattedCreatedAt + " - " + status;
+            // Append all the elements together to form the structure
+            cardBodyDiv.appendChild(senderH5);
+            cardBodyDiv.appendChild(satoshiP);
+            cardBodyDiv.appendChild(receiverH5);
+            cardBodyDiv.appendChild(dateDiv); // add date element
+            cardDiv.appendChild(cardBodyDiv);
 
-        // Append all the elements together to form the structure
-        cardBodyDiv.appendChild(senderH5);
-        cardBodyDiv.appendChild(satoshiP);
-        cardBodyDiv.appendChild(receiverH5);
-        cardBodyDiv.appendChild(dateDiv); // add date element
-        cardDiv.appendChild(cardBodyDiv);
+            // Insert the card div to the body of the document
+            document.getElementById('cards').insertBefore(cardDiv, document.getElementById('cards').firstChild);
 
-        // Insert the card div to the body of the document
-        document.getElementById('cards').insertBefore(cardDiv, document.getElementById('cards').firstChild);
-
-        // Get the username of the sender
-        nostrGetUserinfo(pubkey, divNumber);
+            // Get the username of the sender
+            nostrGetUserinfo(pubkey, divNumber);
+        }
 
     })
     sub.on('eose', () => {
